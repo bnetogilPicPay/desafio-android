c561f830d75c985f56f10e881fd13fb4e089ce3e
Braulio Pinto <braulio.pinto@itbam.org.br>
2021-05-11 18:01:42 -0400

    update gradle file

---
+++ app/src/androidTest/java/com/picpay/desafio/android/ui/activity/MainActivityTest.kt
@@ -0,0 +1,10 @@
+package com.picpay.desafio.android.ui.activity
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MainActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+}
